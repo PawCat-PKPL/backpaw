947c9b95025d99bf683a18dc71ae4f4a488152aa
narawaa <nashwa.ghania@ui.ac.id>
2025-03-26 20:41:40 +0700

    fix: change status code

--- app/admin_dashboard/views.py
+++ app/admin_dashboard/views.py
@@ -1,6 +1,7 @@
 from dateutil.relativedelta import relativedelta
 from django.utils.timezone import now
 
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 
@@ -13,7 +14,7 @@ from utils import api_response
 def user_list(request):
     users = CustomUser.objects.all()
     serializer = UserSerializer(users, many=True)
-    return api_response(200, "List of all users", serializer.data)
+    return api_response(status.HTTP_200_OK, "List of all users", serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
@@ -21,20 +22,20 @@ def inactive_users(request):
     five_months_ago = now() - relativedelta(months=5)
     inactive_users = CustomUser.objects.filter(last_login__lt=five_months_ago)
     serializer = UserSerializer(inactive_users, many=True)
-    return api_response(200, "Users inactive for more than 5 months", serializer.data)
+    return api_response(status.HTTP_200_OK, "Users inactive for more than 5 months", serializer.data)
 
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def delete_user(request, user_id):
     if not isinstance(user_id, int):
-        return api_response(400, "Invalid user ID format")
+        return api_response(status.HTTP_400_BAD_REQUEST, "Invalid user ID format")
     
     try:
         user = CustomUser.objects.get(id=user_id)
         if user.is_superuser or user.is_staff:
-            return api_response(403, "Cannot delete admin or superuser")
+            return api_response(status.HTTP_403_FORBIDDEN, "Cannot delete admin or superuser")
         
         user.delete()
-        return api_response(200, "User successfully deleted")
+        return api_response(status.HTTP_200_OK, "User successfully deleted")
     except CustomUser.DoesNotExist:
-        return api_response(404, "User not found")
+        return api_response(status.HTTP_404_NOT_FOUND, "User not found")
